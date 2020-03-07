bc15d35ff60d507af61872be25f3d58969c055b9
Piotr Gajowniczek <piotrgajow@gmail.com>
2020-03-07 12:47:38 +0100

    Example of template module generated from html

---
+++ test.template.js
@@ -0,0 +1,5 @@
+
+export const template = `
+<h2>test</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci dolore esse molestias nobis reprehenderit soluta. Amet consequatur dignissimos exercitationem explicabo, illo iste iusto laboriosam, laborum, minima minus non ratione recusandae?</p>
+`;
